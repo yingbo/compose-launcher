ac5c466da0c26b59135dfe17c1811b85d24fe405
Yingbo Miao <yingbo@users.noreply.github.com>
2026-02-14 23:42:05 -0500

    release env change (#13)
    * Support .env files per project (#11)
    
    * Support .env files for docker-compose
    
    * Improve .env file picker UI and allow all file types
    
    * Add .env file management UI: edit, replace, remove, and sidebar indicators (#3)
    
    * refactor: Update text color styling and change environment file replacement button icon. (#12)

--- ComposeLauncher/Sources/Models/ComposeFile.swift
+++ ComposeLauncher/Sources/Models/ComposeFile.swift
@@ -4,13 +4,15 @@ struct ComposeFile: Identifiable, Codable, Hashable {
     let id: UUID
     var name: String
     var path: String
+    var envFilePath: String?
     var isRunning: Bool
     var addedDate: Date
     
-    init(id: UUID = UUID(), name: String, path: String, isRunning: Bool = false, addedDate: Date = Date()) {
+    init(id: UUID = UUID(), name: String, path: String, envFilePath: String? = nil, isRunning: Bool = false, addedDate: Date = Date()) {
         self.id = id
         self.name = name
         self.path = path
+        self.envFilePath = envFilePath
         self.isRunning = isRunning
         self.addedDate = addedDate
     }
