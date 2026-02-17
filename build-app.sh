0d098cdbb5ac5beaf720fe33159f733e1f303acd
Yingbo Miao <yingbo@miao.fm>
2026-02-17 18:26:21 -0500

    version 1.1.0

--- build-app.sh
+++ build-app.sh
@@ -52,7 +52,7 @@ cat > "${APP_BUNDLE}/Contents/Info.plist" << EOF
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.0</string>
+    <string>1.1.0</string>
     <key>CFBundleVersion</key>
     <string>1</string>
     <key>LSMinimumSystemVersion</key>
