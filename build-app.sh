714b59422212e0257f25c90a9e7a0753fb093ef2
Yingbo Miao <yingbo@miao.fm>
2026-02-28 02:03:15 -0500

    feat: update build process to copy Info.plist and set version dynamically

--- build-app.sh
+++ build-app.sh
@@ -31,41 +31,8 @@ cp "${BUILD_DIR}/ComposeLauncher" "${APP_BUNDLE}/Contents/MacOS/"
 # Copy icon
 cp "./ComposeLauncher/Sources/Core/Resources/AppIcon.icns" "${APP_BUNDLE}/Contents/Resources/"
 
-# Create Info.plist
-cat > "${APP_BUNDLE}/Contents/Info.plist" << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>CFBundleDevelopmentRegion</key>
-    <string>en</string>
-    <key>CFBundleExecutable</key>
-    <string>ComposeLauncher</string>
-    <key>CFBundleIconFile</key>
-    <string>AppIcon</string>
-    <key>CFBundleIdentifier</key>
-    <string>${BUNDLE_ID}</string>
-    <key>CFBundleInfoDictionaryVersion</key>
-    <string>6.0</string>
-    <key>CFBundleName</key>
-    <string>${APP_NAME}</string>
-    <key>CFBundlePackageType</key>
-    <string>APPL</string>
-    <key>CFBundleShortVersionString</key>
-    <string>1.3.0</string>
-    <key>CFBundleVersion</key>
-    <string>1</string>
-    <key>LSMinimumSystemVersion</key>
-    <string>14.0</string>
-    <key>NSHighResolutionCapable</key>
-    <true/>
-    <key>NSHumanReadableCopyright</key>
-    <string>Copyright © 2026. All rights reserved.</string>
-    <key>LSApplicationCategoryType</key>
-    <string>public.app-category.developer-tools</string>
-</dict>
-</plist>
-EOF
+# Copy Info.plist from source
+cp "./ComposeLauncher/Sources/Core/Resources/Info.plist" "${APP_BUNDLE}/Contents/Info.plist"
 
 # Create PkgInfo
 echo -n "APPL????" > "${APP_BUNDLE}/Contents/PkgInfo"
